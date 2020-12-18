c0c3e779f7d22f5946dc23857095af517b0f30e4
Audrey <audrey.cornaire@gmail.com>
2020-12-18 15:03:50 +0100

    [UPGRADE] Sélection du mode de jeu

--- app.py
+++ app.py
@@ -3,9 +3,19 @@
 
 from src import gameManager
 
-game = gameManager.Morpion()
+choice = 0
 
-game.startGame()
+choice = input("1. Joueur VS Joueur.\n2. Joueur VS Machine.\n3. Machine VS Machine.\nSélectionnez un mode de jeu :")
 
+if choice == "1": 
+    game = gameManager.MorpionHvH()
+    game.startGame()
+elif choice == "2":
+    game = gameManager.MorpionMvH()
+    game.startGame()
+elif choice == "3" :
+    game = gameManager.MorpionMvM()
+    game.startGame()
+else :
+    print("Vous ne savez pas lire, veuillez relancer le jeu.")
 
-https://repl.it/@NathanFallet/MorpionTPE#computer.py
--- src/gameManager.py
+++ src/gameManager.py
@@ -2,7 +2,7 @@ from src import boardManager
 from src import playerManager
 
 
-class Morpion:
+class MorpionMvM:
     def __init__(self):
         self.Board = boardManager.Board()
         self.PlayerX = playerManager.aiPlayer("X")
@@ -33,6 +33,7 @@ class Morpion:
         winner = self.Board.winner()
         if winner == "X":
             rewardX += 3
+            pass
         elif winner == "O":
             rewardO += 3
         elif winner == False:
@@ -42,5 +43,78 @@ class Morpion:
 
         self.Board.getBoardState()
 
-        return rewardX, rewardO
+        return rewardO, rewardX
+
+
+class MorpionMvH:
+    def __init__(self):
+        self.Board = boardManager.Board()
+        self.PlayerX = playerManager.Player("X")
+        self.PlayerO = playerManager.aiPlayer("O")
+
+
+    def startGame(self):
+        self.Board.getBoardState()
+
+        rewardO = 0
+
+        while True:
+            reward = self.PlayerX.turn(self.Board)
+
+            if self.Board.isNotOver():
+                pass
+            else:
+                break
+
+            reward = self.PlayerO.turn(self.Board)
+            rewardO += reward
+            if self.Board.isNotOver():
+                pass
+            else:
+                break
+
+        winner = self.Board.winner()
+        if winner == "X":
+            pass
+        elif winner == "O":
+            rewardO += 3
+        elif winner == False:
+            rewardO += 1
+
+
+        self.Board.getBoardState()
+
+        return rewardO
+
+
+class MorpionHvH:
+    def __init__(self):
+        self.Board = boardManager.Board()
+        self.PlayerX = playerManager.Player("X")
+        self.PlayerO = playerManager.Player("O")
+
+
+    def startGame(self):
+        self.Board.getBoardState()
+
+        while True:
+            reward = self.PlayerX.turn(self.Board)
+
+            if self.Board.isNotOver():
+                pass
+            else:
+                break
+
+            reward = self.PlayerO.turn(self.Board)
+
+            if self.Board.isNotOver():
+                pass
+            else:
+                break
+
+        winner = self.Board.winner()
+
+
+        self.Board.getBoardState()
+
 
